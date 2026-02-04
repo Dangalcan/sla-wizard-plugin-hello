7ccab17562dba3aac6d58acd756bb9597df72729
dangalcan <megamagolas@gmail.com>
2026-02-04 21:06:26 +0100

    feat: make plugin usable as module

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const hello = (options, ctx) => {
+  const greeting = options?.greeting || "Hello";
+  console.log(`${greeting}, ${options.name || 'world'}! 👋`);
+};
+
+module.exports.hello = hello;
+
 module.exports.apply = (program, ctx, config) => {
   program
     .command("hello")
     .description("Command to say hi using a plugin")
     .option("--name <name>", "your name", "world")
     .action((options) => {
-      const greeting = config?.greeting || "Hello";
-      console.log(`${greeting}, ${options.name}! 👋`);
+      const mergedOptions = Object.assign({}, config, options);
+      hello(mergedOptions, ctx);
     });
 };
+
